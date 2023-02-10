f67842e78447d7baa7f2d692ba67c4ace299db8e
Henalu <henaludebarros@hotmail.com>
2023-02-10 11:05:40 +0100

    portfolio styles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,24 +4,35 @@ import { projects } from '../data/projects'
 const Portfolio = () => {
 
   const video = (
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/GhTdJryGjYo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe title='sierra-norte-video' width="100%" height="200" src="https://www.youtube.com/embed/GhTdJryGjYo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   )
-  
+
   return (
-    <div>
+    <div className='portfolio'>
       {
         projects.map((project, index) => {
           return (
-            <article key={index}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h2>{project.title}</h2>
-              </a>
-              <img src={project.image} alt={project.title} />
-              <h3>Categoría: {project.category}</h3>
-              <h4>Tecnologías: {project.technologies}</h4>
-              <p>{project.description}{project.id === 1 && (<div><p>Aquí puedes ver un pequeño video sobre su funcionamiento:</p><br></br><p>{video}</p></div>)}</p>
-              <p>Github: <a href={project.github} target='_blank' rel='noopener noreferrer'>{project.github}</a></p>
-            </article>
+            <div className='item' key={index}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer"><img src={project.image} alt={project.title} /></a>
+              <article>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <h2>{project.title}</h2>
+                </a>
+                <h3>Categoría: {project.category}</h3>
+                <h4>Tecnologías: {project.technologies}</h4>
+                <p>
+                  {project.description}{project.id === 1 && (
+                    <div>
+                      <p>
+                        <a className='link' href='https://www.youtube.com/watch?v=GhTdJryGjYo' target='_blank' rel='noopener noreferrer'>Aquí</a> puedes ver un pequeño video sobre su funcionamiento:
+                      </p>
+                      <p className='video-container'>{video}</p>
+                    </div>
+                  )}
+                </p>
+                <p className='github'>Para echarle un vistazo al código entra en <a href={project.github} target='_blank' rel='noopener noreferrer'>GitHub</a></p>
+              </article>
+            </div>
           )
         })
       }
